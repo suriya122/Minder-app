29d600e367066c61c4f176e110cd6dac327d111d
suriyaa <suriyaashine8072@gmail.com>
2021-03-29 21:12:43 +0530

    Updates

---
+++ precache-manifest.3b32e00844074a394acd4b93f33cea21.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1236b0e244a8e07eae53031481d68ead",
+    "url": "/Minder-app/index.html"
+  },
+  {
+    "revision": "abe90bf5957d6024e3ee",
+    "url": "/Minder-app/static/js/2.d37a1a21.chunk.js"
+  },
+  {
+    "revision": "7c000872728450c24b98",
+    "url": "/Minder-app/static/js/main.e532cf8f.chunk.js"
+  },
+  {
+    "revision": "35bf929bc3f0ac63530c",
+    "url": "/Minder-app/static/js/runtime-main.dd292e38.js"
+  }
+]);
